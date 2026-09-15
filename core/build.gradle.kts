d6dc35e0e34c356a4d8625a36d4fa792a4070c4c
couam <nawak12@gmail.com>
2026-09-15 21:53:18 +0200

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     }
 
     defaultConfig {
-        minSdk = 23
+        minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
